6dc521c02a1b4ae2e9c742c3f09e26a161fd08fe
BenRangasamy <60889975+BenRangasamy@users.noreply.github.com>
2022-03-10 12:28:15 +0000

    E2E tests for rings/quadrant explanation functionality added

--- e2e-tests/page-content.spec.js
+++ e2e-tests/page-content.spec.js
@@ -3,6 +3,12 @@ let radar;
 let legendItem10;
 let bubble;
 let consoleLogMessage;
+let ringsHeading;
+let quadrantsHeading;
+let ringsTextSection;
+let quadrantsTextSection;
+let quadrantsText;
+let ringsText;
 
 // User goes to webpage
 test.beforeEach(async ({ page }) => {
@@ -10,6 +16,12 @@ test.beforeEach(async ({ page }) => {
     radar = await page.locator('#radar');
     legendItem10 = await page.locator('#legendItem10');
     bubble = await page.locator("#bubble");
+    ringsHeading = await page.locator("#rings-heading");
+    quadrantsHeading = await page.locator('#quadrants-heading');
+    ringsTextSection = await page.locator('#rings-text-section');
+    quadrantsText = await page.locator('.quadrants-text');
+    quadrantsTextSection = await page.locator('#quadrants-text-section');
+    ringsText = await page.locator('.rings-text');
     consoleLogMessage = null;
     await page.on("console", (msg) => {
         consoleLogMessage = msg;
@@ -32,6 +44,31 @@ test.describe('Hovering over a label in the legend', () => {
     test('to check that hovering over an item displays a label on the visualisation', async ({ page }) => {
         await page.hover("#legendItem10", {timeout: 20000});
         await expect(bubble).toBeVisible();
+    });
+});
+
+test.describe('User wants to know what the rings and quadrants are referring to', () => {
+    test('that there is a button for users who want to know what the rings are', () => {
+        await expect(ringsHeading).toHaveText("What are the rings?");
+    });
+    test('that there is a button for users who want to know what the quadrants are', () => {
+        await expect(quadrantsHeading).toHaveText("What are the quadrants?");
+    });
+    test('that the section of text explaining the rings is initially hidden', () => {
+        await expect(ringsTextSection).not.tobeVisible();
+    });
+    test('that the section of text explaining the quadrants is initially hidden', () => {
+        await expect(quadrantsTextSection).not.tobeVisible();
+    });
+    test('that a section of text explains the rings when the heading is clicked', () => {
+        await page.click('#rings-heading');
+        await expect(ringsTextSection).tobeVisible();
+        await expect(ringsText.first()).not.toBeEmpty();
+    });
+    test('that a section of text explains the quadrants when the heading is clicked', () => {
+        await page.click('quadrants-heading');
+        await expect(quadrantsTextSection).tobeVisible();
+        await expect(quadrantsText.first()).not.toBeEmpty();
     });
 });
 
