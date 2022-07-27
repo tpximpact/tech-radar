f91deceafec6b696e636a1d9791ef7318331dd5f
Sarah Williams <sarah.a.williams@tpximpact.com>
2022-07-27 15:00:36 +0100

    Tweaked styling of info section

--- public/main.js
+++ public/main.js
@@ -2,10 +2,10 @@ import { radar_visualization } from './build-radar.js';
 import { buildFormattedInnerHTMLFrom } from './main-helper.js';
 
 // Show loading animation
-const loader = document.getElementById("loader-container");
-const svgWrapper = document.getElementById("svg-wrapper");
-svgWrapper.style.display = "none";
-loader.style.display = "grid";
+const loader = document.getElementById('loader-container');
+const svgWrapper = document.getElementById('svg-wrapper');
+svgWrapper.style.display = 'none';
+loader.style.display = 'grid';
 
 const getRadarData = async () => {
     const result = await fetch(`${window.location.origin}/radar`);
@@ -14,7 +14,9 @@ const getRadarData = async () => {
 };
 
 const getRingsAndQuadrantsInfo = async () => {
-    const result = await fetch(`${window.location.origin}/rings-and-quadrants-info`);
+    const result = await fetch(
+        `${window.location.origin}/rings-and-quadrants-info`
+    );
     const data = await result.json();
     return data;
 };
@@ -23,17 +25,18 @@ const data = await getRadarData();
 const ringsAndQuadrantInfo = await getRingsAndQuadrantsInfo();
 
 // Hide loading animation
-loader.style.display = "none";
-svgWrapper.style.display = "block";
+loader.style.display = 'none';
+svgWrapper.style.display = 'block';
 
 function redraw() {
     let svgWrapperWidth = svgWrapper.offsetWidth;
 
     // Set height of SVG wrapper so that items below it respond to the change of height
-    document.getElementById("svg-wrapper").style.height = (svgWrapperWidth * 0.7).toString() + "px";
+    document.getElementById('svg-wrapper').style.height =
+        (svgWrapperWidth * 0.7).toString() + 'px';
 
     // Clear SVG
-    let svg = document.getElementById("radar");
+    let svg = document.getElementById('radar');
     while (svg.lastChild) {
         svg.removeChild(svg.lastChild);
     }
@@ -43,7 +46,7 @@ function redraw() {
         svg_id: 'radar',
         width: svgWrapperWidth,
         height: 1000,
-        viewbox: "0 0 " + (svgWrapperWidth) + " " + (1400000/svgWrapperWidth),
+        viewbox: '0 0 ' + svgWrapperWidth + ' ' + 1400000 / svgWrapperWidth,
         colors: {
             background: '#fff',
             grid: '#bbb',
@@ -70,17 +73,27 @@ function redraw() {
 Display excluded items to user
 */
 if (data.excludedData.length > 0) {
-    document.getElementById('incomplete-data-warning').style.visibility = "visible";
+    document.getElementById('incomplete-data-warning').style.visibility =
+        'visible';
     let hiddenItemsList = document.getElementById('hidden-items-list');
     for (const excludedItem of data.excludedData) {
         let newListItem = document.createElement('li');
-        newListItem.className = "list-disc";
-        let missingAttributes = "";
+        newListItem.className = 'list-disc';
+        let missingAttributes = '';
         for (const missingAttribute of excludedItem.invalidAttributes) {
-            missingAttributes += missingAttribute + ", "
+            missingAttributes += missingAttribute + ', ';
         }
-        missingAttributes = missingAttributes.substring(0, missingAttributes.length - 2);
-        newListItem.appendChild(document.createTextNode(excludedItem.label + " is missing values for " + missingAttributes));
+        missingAttributes = missingAttributes.substring(
+            0,
+            missingAttributes.length - 2
+        );
+        newListItem.appendChild(
+            document.createTextNode(
+                excludedItem.label +
+                    ' is missing values for ' +
+                    missingAttributes
+            )
+        );
         hiddenItemsList.appendChild(newListItem);
     }
 }
@@ -88,15 +101,23 @@ if (data.excludedData.length > 0) {
 redraw();
 
 // Display rings and quadrant info
-let ringsHeading = document.getElementById("rings-heading");
-let quadrantsHeading = document.getElementById("quadrants-heading");
-let ringsTextSection = document.getElementById("rings-text-section");
-let quadrantsTextSection = document.getElementById("quadrants-text-section");
+let ringsHeading = document.getElementById('rings-heading');
+let quadrantsHeading = document.getElementById('quadrants-heading');
+let ringsTextSection = document.getElementById('rings-text-section');
+let quadrantsTextSection = document.getElementById('quadrants-text-section');
 
-ringsHeading.innerHTML = buildFormattedInnerHTMLFrom(ringsAndQuadrantInfo.ringsSection[0].textArray);
-quadrantsHeading.innerHTML = buildFormattedInnerHTMLFrom(ringsAndQuadrantInfo.quadrantsSection[0].textArray);
+ringsHeading.innerHTML = buildFormattedInnerHTMLFrom(
+    ringsAndQuadrantInfo.ringsSection[0].textArray
+);
+quadrantsHeading.innerHTML = buildFormattedInnerHTMLFrom(
+    ringsAndQuadrantInfo.quadrantsSection[0].textArray
+);
 
-const populateBulletpointTextSection = (bulletpointTextSection, bulletpointTextArray, className) => {
+const populateBulletpointTextSection = (
+    bulletpointTextSection,
+    bulletpointTextArray,
+    className
+) => {
     /*
         bulletpointTextSection: A DOM element to populate with bullet points.
         bulletpointTextArray:   An array containing input data from the Notion API containing only data
@@ -104,36 +125,26 @@ const populateBulletpointTextSection = (bulletpointTextSection, bulletpointTextA
     */
     for (const bulletpoint of bulletpointTextArray) {
         const innerHTML = buildFormattedInnerHTMLFrom(bulletpoint.textArray);
-        const tag = document.createElement("div");
-        tag.style.padding = "1em";
+        const tag = document.createElement('li');
         tag.innerHTML = innerHTML;
         tag.className = className;
         bulletpointTextSection.appendChild(tag);
     }
 };
 
-populateBulletpointTextSection(ringsTextSection, ringsAndQuadrantInfo.ringsSection.slice(1), 'rings-text');
-populateBulletpointTextSection(quadrantsTextSection, ringsAndQuadrantInfo.quadrantsSection.slice(1), 'quadrants-text');
-
-// When ring/quadrants headings are clicked, the relevant section is made visible
-ringsHeading.onclick = () => {
-    if (ringsTextSection.style.lineHeight == "0") {
-        ringsTextSection.style = "overflow: hidden; transition: all .5s ease-in-out; line-height: 1.5; padding: 0 1em; padding-top: 1em; padding-bottom: 1em; color: black;";
-    } else {
-        ringsTextSection.style = "overflow: hidden; transition: all .5s ease-in-out; line-height: 0; padding: 0 1em; color: transparent;";
-    }
-};
-
-quadrantsHeading.onclick = () => {
-    if (quadrantsTextSection.style.lineHeight == "0") {
-        quadrantsTextSection.style = "overflow: hidden; transition: all .5s ease-in-out; line-height: 1.5; padding: 0 1em; padding-top: 1em; padding-bottom: 1em; color: black;";
-    } else {
-        quadrantsTextSection.style = "overflow: hidden; transition: all .5s ease-in-out; line-height: 0; padding: 0 1em; color: transparent;";
-    }
-};
+populateBulletpointTextSection(
+    ringsTextSection,
+    ringsAndQuadrantInfo.ringsSection.slice(1),
+    'pb-2'
+);
+populateBulletpointTextSection(
+    quadrantsTextSection,
+    ringsAndQuadrantInfo.quadrantsSection.slice(1),
+    'pb-2'
+);
 
 window.onresize = () => {
     setTimeout(() => {
         redraw();
-    }, 100)
+    }, 100);
 };
